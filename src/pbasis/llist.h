690746ee73df7c8aae8efbbb282d050cb61dac51
Jan Max Meyer <none@none>
2011-04-07 02:49:13 +0200

    SCCS llist.h 09/07/08 11:09:40: date and time created 09/07/08 11:09:40 by neo

---
+++ src/pbasis/llist.h
@@ -0,0 +1,39 @@
+/* -HEADER----------------------------------------------------------------------
+Phorward Basis Library :: Linked Pointer Lists
+Copyright (C) 2006-2009 by Phorward Software Technologies, Jan Max Meyer
+http://www.phorward-software.com ++ contact<at>phorward-software<dot>com
+
+File:	llist.h
+Usage:	Universal management functions for dynamic linked pointer list
+----------------------------------------------------------------------------- */
+
+#ifndef LLIST_H
+#define LLIST_H
+
+/* Structures */
+typedef struct llist
+{
+	void*			pptr;
+	struct llist*	next;
+} LIST;
+
+/* Defines for old-style function names */
+#define listPush				list_push
+#define listPop					list_pop
+#define listRemove				list_remove
+#define listFree				list_free
+#define listPrint				list_print
+#define listDup					list_dup
+#define listFind				list_find
+#define listGetPtrByPosition	list_getptr
+#define listDiff				list_diff
+#define listUnion				list_union
+#define listCount				list_count
+#define listSubset				list_subset
+
+/* Macros */
+#define list_access( ll )		( (ll) ? (ll)->pptr : (void*)NULL )
+#define list_next( ll )			( (ll) ? (ll)->next : (LIST*)NULL )
+
+#endif
+
