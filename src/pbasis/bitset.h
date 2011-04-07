d239bef4e4467f93c2360a8021d25e0af7d53390
Jan Max Meyer <none@none>
2011-04-07 02:48:54 +0200

    SCCS bitset.h 09/07/08 11:09:29: date and time created 09/07/08 11:09:29 by neo

---
+++ src/pbasis/bitset.h
@@ -0,0 +1,21 @@
+/* -HEADER----------------------------------------------------------------------
+Phorward Basis Library :: Bitset Functions
+Copyright (C) 2006-2009 by Phorward Software Technologies, Jan Max Meyer
+http://www.phorward-software.com ++ contact<at>phorward-software<dot>com
+
+File:	bitset.h
+Author:	Jan Max Meyer
+Usage:	Bitset library header
+----------------------------------------------------------------------------- */
+
+#ifndef BITSET_H
+#define BITSET_H
+
+/* Typedefs */
+typedef uchar byte;
+typedef byte* bitset;
+
+/* Macros */
+#define bitset_free( set ) 		free( set )
+
+#endif
