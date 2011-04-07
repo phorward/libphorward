1cf7898597143612a757f16a664d54c97d132494
Jan Max Meyer <none@none>
2011-04-07 02:50:21 +0200

    SCCS dmake.bat 10/06/22 08:50:27: Replaced script headers for Open Source release

--- run/dmake.bat
+++ run/dmake.bat
@@ -1,4 +1,14 @@
 @echo off
+rem ----------------------------------------------------------------------------
+rem Phorward Software Development Kit
+rem Copyright (C) 2009, 2010 by Phorward Software Technologies, Jan Max Meyer
+rem http://www.phorward-software.com ++ contact<at>phorward<dash>software<dot>com
+rem All rights reserved. See $PHOME/LICENSE for more information.
+rem
+rem Script:		dmake.bat
+rem Author:		Jan Max Meyer
+rem Usage:		Run 'make' with Debug-Flag
+rem ----------------------------------------------------------------------------
 echo ********************
 echo * DEBUG MAKE START *
 echo ********************
