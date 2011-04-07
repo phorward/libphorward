587c13169cbbc8f8e2e5224f8d4d2776ac847b10
Jan Max Meyer <none@none>
2011-04-07 02:50:02 +0200

    SCCS util.c 09/12/05 04:44:58: Snapshot, bug-freed version of pregex v2

--- src/pregex/util.c
+++ src/pregex/util.c
@@ -35,6 +35,8 @@ Usage:	Utility functions for additional usage that belong to the regex library
 					uchar*			str			The sequence of chatacters to
 												be converted one-to-one into an
 												NFA.
+					int				flags		Flags 
+					int				acc			Acceping identifier
 																	
 	Returns:		int							ERR_OK on success,
 												ERR... error code else
@@ -42,49 +44,89 @@ Usage:	Utility functions for additional usage that belong to the regex library
 	~~~ CHANGES & NOTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	Date:		Author:			Note:
 ----------------------------------------------------------------------------- */
-int pregex_nfa_from_string( pregex_nfa* nfa, uchar* str )
+int pregex_nfa_from_string( pregex_nfa* nfa, uchar* str, int flags, int acc )
 {
 	pregex_nfa_st*	nfa_st;
+	pregex_nfa_st*	append_to;
 	pregex_nfa_st*	first_nfa_st;
 	pregex_nfa_st*	prev_nfa_st;
 	uchar*			pstr;
+	wchar			ch;
 
 	PROC( "pregex_nfa_from_string" );
 	PARMS( "nfa", "%p", nfa );
 	PARMS( "str", "%s", str );
+	PARMS( "flags", "%d", flags );
+	PARMS( "acc", "%d", acc );
 
 	if( !( nfa && str ) )
 		RETURN( ERR_PARMS );
 
+	/* Find node to integrate into existing machine */
+	for( append_to = (pregex_nfa_st*)list_access( nfa->states );
+		append_to && append_to->next2; append_to = append_to->next2 )
+			/* Find last first node ;) ... */ ;
+
 	/* Create first state - this is an epsilon node */
 	if( !( first_nfa_st = prev_nfa_st =
-			pregex_nfa_create_state( nfa, (uchar*)NULL ) ) )
+			pregex_nfa_create_state( nfa, (uchar*)NULL, flags ) ) )
 		RETURN( ERR_MEM );
 
-	for( pstr = str; *pstr; pstr += u8_seqlen( pstr ) )
+	for( pstr = str; *pstr; )
 	{
 		/* Then, create all states that form the string */
 		MSG( "Adding new state" );
 		VARS( "pstr", "%s", pstr );
-		if( !( nfa_st = pregex_nfa_create_state( nfa, (uchar*)NULL ) ) )
+		if( !( nfa_st = pregex_nfa_create_state(
+				nfa, (uchar*)NULL, flags ) ) )
 			RETURN( ERR_MEM );
 
-		VARS( "adding char", "%d", u8_char( pstr ) );
-		if( !( nfa_st->ccl = ccl_addrange(
-				(CCL)NULL, u8_char( pstr ), u8_char( pstr ) ) ) )
+		ch = u8_parse_char( &pstr );
+
+		VARS( "ch", "%d", ch );
+		if( !( nfa_st->ccl = ccl_addrange( (CCL)NULL, ch, ch ) ) )
 			RETURN( ERR_MEM );
+
+		/* Is case-insensitive flag set? */
+		if( flags & REGEX_MOD_INSENSITIVE )
+		{
+#ifdef UTF8
+			MSG( "UTF-8 mode, trying to convert" );
+			if( iswupper( ch ) )
+				ch = towlower( ch );
+			else
+				ch = towupper( ch );
+#else
+			MSG( "non UTF-8 mode, trying to convert" );
+			if( isupper( ch ) )
+				ch = (uchar)tolower( (int)ch );
+			else
+				ch = (uchar)toupper( (int)ch );
+#endif
+
+			MSG( "Case-insensity set, new character evaluated is:" );
+			VARS( "ch", "%d", ch );
+			if( !( nfa_st->ccl = ccl_addrange( nfa_st->ccl, ch, ch ) ) )
+				RETURN( ERR_MEM );
+		}
 
 		prev_nfa_st->next = nfa_st;
 		prev_nfa_st = nfa_st;
 	}
 
-	/* Integrate into existing machine */
-	for( nfa_st = (pregex_nfa_st*)list_access( nfa->states );
-		nfa_st && nfa_st->next2; nfa_st = nfa_st->next2 )
-			/* Find last first node ;) ... */ ;
+	/* Add accepting node */
+	VARS( "acc", "%d", acc );
+	if( !( nfa_st = pregex_nfa_create_state( nfa,
+			(uchar*)NULL, flags ) ) )
+		RETURN( ERR_MEM );
 
-	if( nfa_st )
-		nfa_st->next = first_nfa_st;
+	nfa_st->accept = acc;
+	prev_nfa_st->next = nfa_st;
+
+	/* Append to existing machine, if required */
+	VARS( "append_to", "%p", append_to );
+	if( append_to )
+		append_to->next2 = first_nfa_st;
 		
 	RETURN( ERR_OK );
 }
