effd29d9b1f4cb19b5fd9e2d76cdafc1fc04a2b2
Jan Max Meyer <none@none>
2011-04-07 02:49:50 +0200

    SCCS direct.c 09/10/23 23:59:23: date and time created 09/10/23 23:59:23 by neo

---
+++ src/pregex/direct.c
@@ -0,0 +1,73 @@
+/* -MODULE----------------------------------------------------------------------
+Phorward Regular Expression Library, Version 2
+Copyright (C) 2009 by Phorward Software Technologies, Jan Max Meyer
+http://www.phorward-software.com ++ mail<at>phorward<dash>software<dot>com
+
+File:	direct.c
+Author:	Jan Max Meyer
+Usage:	Direct regular expression handling functions
+----------------------------------------------------------------------------- */
+
+/*
+ * Includes
+ */
+#include "pregex.h"
+
+/*
+ * Global variables
+ */
+
+/*
+ * Functions
+ */
+
+int pregex_match( uchar* regex, uchar* str, pregex_result** results )
+{
+	int				matches	= 0;
+	int				ret;
+	u_int			len;
+	uchar*			pstr;
+	pregex_nfa		nfa;
+	pregex_result	res;
+
+	PROC( "pregex_match" );
+	PARMS( "regex", "%s", regex );
+	PARMS( "str", "%s", str );
+	PARMS( "results", "%p", results );
+
+	memset( &nfa, 0, sizeof( pregex_nfa ) );
+	if( ( ret = parse_expr( regex, &nfa, 0 ) ) != ERR_OK ) 
+		RETURN( ret );
+
+	for( pstr = str; *pstr; )
+	{
+		VARS( "pstr", "%s", pstr );
+		if( pregex_nfa_match( &nfa, pstr, &len ) == 0 )
+		{
+			if( !matches )
+				*results = (pregex_result*)pmalloc( sizeof( pregex_result ) );
+			else
+				*results = (pregex_result*)prealloc(
+								(pregex_result*)*results,
+									( matches + 1 ) * sizeof( pregex_result ) );
+
+			if( !*results )
+				RETURN( ERR_MEM );
+
+			(*results)[ matches ].begin = pstr;
+
+			for( ; len > 0; len-- )
+				pstr += u8_seqlen( pstr );
+
+			(*results)[ matches ].end = pstr;
+			matches++;
+		}
+		else
+			pstr += u8_seqlen( pstr );
+
+	}
+	
+
+	RETURN( matches );
+}
+
