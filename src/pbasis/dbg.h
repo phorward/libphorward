0fe65d7466c2e777488f1f87e9167dac2e35fabf
Jan Max Meyer <none@none>
2011-04-07 02:49:05 +0200

    SCCS dbg.h 10/06/22 00:19:41: Header changes for Open Source release

--- src/pbasis/dbg.h
+++ src/pbasis/dbg.h
@@ -1,7 +1,8 @@
 /* -HEADER----------------------------------------------------------------------
-Phorward Basis Library :: Trace and Debug Library
-Copyright (C) 2006-2009 by Phorward Software Technologies, Jan Max Meyer
-http://www.phorward-software.com ++ contact<at>phorward-software<dot>com
+Phorward Foundation Libraries :: Basis Library
+Copyright (C) 2006-2010 by Phorward Software Technologies, Jan Max Meyer
+http://www.phorward-software.com ++ contact<at>phorward<dash>software<dot>com
+All rights reserved. See $PHOME/LICENSE for more information.
 
 File:	dbg.h
 Usage:	Program Trace Facilities
