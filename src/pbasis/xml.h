406100ad3b0a9804b7086da1e4062fffa5a4ef73
Jan Max Meyer <none@none>
2011-04-07 02:49:41 +0200

    SCCS xml.h 10/09/03 08:14:27: date and time created 10/09/03 08:14:27 by neo

---
+++ src/pbasis/xml.h
@@ -0,0 +1,80 @@
+/* -HEADER----------------------------------------------------------------------
+Phorward Foundation Libraries :: Basis Library
+Copyright (C) 2006-2010 by Phorward Software Technologies, Jan Max Meyer
+http://www.phorward-software.com ++ contact<at>phorward<dash>software<dot>com
+All rights reserved. See $PHOME/LICENSE for more information.
+
+File:	xml.h
+Author:	Aaron Voisine, contributions by Jan Max Meyer
+Usage:	XML processing functions (based on ezXML)
+----------------------------------------------------------------------------- */
+
+/* xml.h
+ *
+ * Copyright 2004-2006 Aaron Voisine <aaron@voisine.org>
+ *
+ * Permission is hereby granted, free of ucharge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef _XML_H
+#define _XML_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define XML_BUFSIZE 1024 /* size of internal memory buffers */
+#define XML_NAMEM   0x80 /* name is malloced */
+#define XML_TXTM    0x40 /* txt is malloced */
+#define XML_DUP     0x20 /* attribute name and value are strduped */
+
+typedef struct xml *XML_T;
+struct xml
+{
+    uchar *name;      /* tag name */
+    uchar **attr;     /* tag attributes { name, value, name, value, ... NULL } */
+    uchar *txt;       /* tag character content, empty string if none */
+    size_t off;      /* tag offset from start of parent tag character content */
+    XML_T next;    /* next tag with same name in this section at this depth */
+    XML_T sibling; /* next tag with different name in same section and depth */
+    XML_T ordered; /* next tag, same section and depth, in original order */
+    XML_T child;   /* head of sub tag list, NULL if none */
+    XML_T parent;  /* parent tag, NULL if current tag is root tag */
+    short flags;     /* additional information */
+};
+
+#define xml_next(xml) ((xml) ? xml->next : NULL)
+#define xml_name(xml) ((xml) ? xml->name : NULL)
+#define xml_txt(xml) ((xml) ? xml->txt : "")
+#define xml_new_d(name) xml_set_flag(xml_new(strdup(name)), XML_NAMEM)
+#define xml_add_child_d(xml, name, off) \
+    xml_set_flag(xml_add_child(xml, strdup(name), off), XML_NAMEM)
+#define xml_set_txt_d(xml, txt) \
+    xml_set_flag(xml_set_txt(xml, strdup(txt)), XML_TXTM)
+#define xml_set_attr_d(xml, name, value) \
+    xml_set_attr(xml_set_flag(xml, XML_DUP), strdup(name), strdup(value))
+#define xml_move(xml, dest, off) xml_insert(xml_cut(xml), dest, off)
+#define xml_remove(xml) xml_free(xml_cut(xml))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _XML_H */
