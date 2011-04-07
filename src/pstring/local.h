98fb99e5e0a734c032f086d22b85a020a6ab2959
Jan Max Meyer <none@none>
2011-04-07 02:50:05 +0200

    SCCS local.h 10/01/28 09:53:52: date and time created 10/01/28 09:53:52 by neo

---
+++ src/pstring/local.h
@@ -0,0 +1,35 @@
+/* -HEADER----------------------------------------------------------------------
+Phorward String Object Library
+Copyright (C) 2010 by Phorward Software Technologies, Jan Max Meyer
+http://www.phorward-software.com ++ contact<at>phorward<dash>software<dot>com
+
+File:	local.h
+Author:	Jan Max Meyer
+Usage:	Some local macros and declarations used by Pstring-library
+----------------------------------------------------------------------------- */
+
+#ifndef PSTRING_LOCAL_H
+#define PSTRING_LOCAL_H
+
+/* 
+ * memory (re)allocation is done in 16-character blocks
+ */
+#define MALLOC_STEP				16
+
+/* 
+ * numeric values are hold in a length of 128 chars?
+ * must be enough...
+ */
+#define NUMERIC_VALUE_LEN		128
+
+/*
+ * macros for correct memory size computation
+ */
+#define GET_ALLOC_SIZE( size )	( ( (size) ? (size) + ( (size) % MALLOC_STEP ) \
+									: MALLOC_STEP ) + 1 )
+#define ALLOC_STR( obj, len )	( obj->str = (pchar*)pmalloc( \
+									GET_ALLOC_SIZE( len ) \
+										* sizeof( pchar ) ) )
+
+#endif
+
