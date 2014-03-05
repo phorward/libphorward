cd6ce6178c6a806b1205612e91fba371fd000286
Jan Max Meyer <none@none>
2014-03-05 08:06:19 +0100

    Added missing file ll.h

---
+++ src/parser/ll.h
@@ -0,0 +1,23 @@
+/* -HEADER----------------------------------------------------------------------
+Phorward Foundation Toolkit
+Copyright (C) 2006-2014 by Phorward Software Technologies, Jan Max Meyer
+http://www.phorward-software.com ++ contact<at>phorward<dash>software<dot>com
+All rights reserved. See LICENSE for more information.
+
+File:	ll.h
+Usage:	LL parsing related
+----------------------------------------------------------------------------- */
+
+/*
+typedef struct
+{
+	pgnonterminal*	nonterminal;
+	plist*			trans;
+} pgllstate;
+
+typedef struct
+{
+	pgterminal*		terminal;
+	pgproduction*	production;
+} pglltrans;
+*/
