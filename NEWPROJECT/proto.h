0a130d67eff5d59a16e412211f9800237c5bd016
Jan Max Meyer <none@none>
2014-06-29 16:48:13 +0200

    Removed the files from NEWPROJECT, I don't think these are necessary anmore.

--- NEWPROJECT/proto.h
+++
@@ -1,9 +0,0 @@
-/*
- * This is a generated file - manual editing is not recommended!
- */
-
-/* main.c */
-void copyright( void );
-void usage( char* progname );
-BOOLEAN get_command_line( int argc, char** argv );
-
