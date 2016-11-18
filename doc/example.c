a1f95dd90fe0305f9c2dbae9f405bf71dcb2cce3
Jan Max Meyer <jmm@phorward.de>
2016-11-18 08:40:17 +0100

    Updating the example codes.
    --HG--
    branch : bnfrework

--- doc/example.c
+++ doc/example.c
@@ -7,15 +7,14 @@ int main()
     char*   input = "1+2*(3+4)+5";
     char*   end;
 
-    parser = pp_create( 0,  "%emitall"
-
-                            "factor: /[0-9]+/ | '(' expr ')' ;"
-                            "term:   term '*' factor | factor ;"
-                            "expr:   expr '+' term | term ;" );
+    parser = pp_create( 0,  "@int /[0-9]+/ ;"
+							"fact : int | '(' expr ')' ;"
+                            "term : @mul( term '*' fact ) | fact ;"
+                            "expr : @add( expr '+' term ) | term ;" );
 
     if( !pp_parse_to_ast( &ast, parser, input, &end ) )
         return 1; /* parse error */
 
-    pp_ast_simplify( ast );
+    pp_ast_dump_short( stdout, ast );
     return 0;
 }
