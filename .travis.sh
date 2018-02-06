e72c34dd9663c7528339dc74847828ace3b1acd2
Jan Max Meyer <jmm@phorward.de>
2018-02-06 12:18:09 +0100

    another try

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 ./configure
-CFLAGS="-g" make
+CFLAGS="-DDEBUG" make
 sudo make install
 #pparse -G "f: /[0-9]+/@int | '(' e ')' ; mul@: t '*' f ; t: mul@ | f; add@: e '+' t; e: add@ | t ;"  "1+2*3+4*5"
 #pparse -G "a: Integer; b: 'Hallo Welt'; c: \"Tri tra\"; d: /[0-9]+/@int ;"  "1234"
