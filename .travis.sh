3a49a0e1256e525d60de2a274b4ed5908c491a99
Jan Max Meyer <jmm@phorward.de>
2018-02-06 12:28:16 +0100

    I need trace

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
-./configure
-CFLAGS="-DDEBUG" make
+./configure --with-trace
+make
 sudo make install
 #pparse -G "f: /[0-9]+/@int | '(' e ')' ; mul@: t '*' f ; t: mul@ | f; add@: e '+' t; e: add@ | t ;"  "1+2*3+4*5"
 #pparse -G "a: Integer; b: 'Hallo Welt'; c: \"Tri tra\"; d: /[0-9]+/@int ;"  "1234"
